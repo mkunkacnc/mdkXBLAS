598274d12ea277a58b89401b6352c7d674b76c14
Mark Kunka <mkunka@proton.me>
2025-12-22 00:43:55 +0000

    change variable names.

--- common/blas_doubledouble.c
+++ common/blas_doubledouble.c
@@ -25,34 +25,34 @@ void compute_doubledouble_eq_double_mul_double(double* head_c,
 }
 
 /* compute c = a * b */
-void compute_doubledouble_eq_doubledouble_mul_double(double* head_t1,
-                                                     double* tail_t1,
-                                                     double head_a0,
-                                                     double tail_a0,
-                                                     double cd)
+void compute_doubledouble_eq_doubledouble_mul_double(double* head_c,
+                                                     double* tail_c,
+                                                     double head_a,
+                                                     double tail_a,
+                                                     double b)
 {
   /* Compute double-double = double-double * double. */
   double a11, a21, b1, b2, c11, c21, c2, con, t1, t2;
 
-  con = head_a0 * SPLIT;
-  a11 = con - head_a0;
+  con = head_a * SPLIT;
+  a11 = con - head_a;
   a11 = con - a11;
-  a21 = head_a0 - a11;
+  a21 = head_a - a11;
 
-  con = cd * SPLIT;
-  b1 = con - cd;
+  con = b * SPLIT;
+  b1 = con - b;
   b1 = con - b1;
-  b2 = cd - b1;
+  b2 = b - b1;
 
-  c11 = head_a0 * cd;
+  c11 = head_a * b;
   c21 = (((a11 * b1 - c11) + a11 * b2) + a21 * b1) + a21 * b2;
 
-  c2 = tail_a0 * cd;
+  c2 = tail_a * b;
   t1 = c11 + c2;
   t2 = (c2 - (t1 - c11)) + c21;
 
-  *head_t1 = t1 + t2;
-  *tail_t1 = t2 - (*head_t1 - t1);
+  *head_c = t1 + t2;
+  *tail_c = t2 - (*head_c - t1);
 }
 
 /* compute c = a + b */
@@ -123,20 +123,20 @@ double compute_double_eq_doubledouble_add_doubledouble(double head_a,
 }
 
 /* compute c = a + b */
-void compute_doubledouble_eq_double_add_double(double* head_e1,
-                                               double* tail_e1,
-                                               double d1,
-                                               double d2)
+void compute_doubledouble_eq_double_add_double(double* head_c,
+                                               double* tail_c,
+                                               double a,
+                                               double b)
 {
   /* Compute double-double = double + double. */
   double e, t1, t2;
 
   /* Knuth trick. */
-  t1 = d1 + d2;
-  e = t1 - d1;
-  t2 = ((d2 - e) + (d1 - (t1 - e)));
+  t1 = a + b;
+  e = t1 - a;
+  t2 = ((b - e) + (a - (t1 - e)));
 
   /* The result is t1 + t2, after normalization. */
-  *head_e1 = t1 + t2;
-  *tail_e1 = t2 - (*head_e1 - t1);
+  *head_c = t1 + t2;
+  *tail_c = t2 - (*head_c - t1);
 }
